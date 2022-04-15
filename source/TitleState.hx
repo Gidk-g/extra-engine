1c11a1214377f63290701ded1ccc859abee92a58
Gidk-g <gidkg302@gmail.com>
2022-04-15 20:38:45 +0200

    fuck you OutdatedSubState

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -291,17 +291,6 @@ class TitleState extends MusicBeatState
 				// Check if version is outdated
 
 				var version:String = "v" + Application.current.meta.get('version');
-
-				if (version.trim() != NGio.GAME_VER_NUMS.trim() && !OutdatedSubState.leftState)
-				{
-					FlxG.switchState(new OutdatedSubState());
-					trace('OLD VERSION!');
-					trace('old ver');
-					trace(version.trim());
-					trace('cur ver');
-					trace(NGio.GAME_VER_NUMS.trim());
-				}
-				else
 				{
 					FlxG.switchState(new MainMenuState());
 				}
