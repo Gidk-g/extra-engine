c51e824249263b28c6f049d6810a534a1fd8b034
Gidk-g <gidkg302@gmail.com>
2022-04-15 14:37:26 +0200

    code

---
+++ source/OptionsMenu.hx
@@ -0,0 +1,123 @@
+package;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+using StringTools;
+
+class OptionsMenu extends MusicBeatState
+{
+	var options:Array<String> = ['Controls'];
+	private var grpOptions:FlxTypedGroup<Alphabet>;
+	private static var curSelected:Int = 0;
+	public static var instance:OptionsMenu;
+	public var acceptInput:Bool = true;
+	public static var menuBG:FlxSprite;
+
+	function openSelectedSubstate(label:String) {
+		switch(label) {
+			case 'Controls':
+				openSubState(new KeyBindMenu());
+		}
+	}
+
+	override function create() {
+		instance = this;
+		#if desktop
+		DiscordClient.changePresence("Options Menu", null);
+		#end
+
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.color = 0xFFea71fd;
+		bg.updateHitbox();
+
+		bg.screenCenter();
+		bg.antialiasing = false;
+		add(bg);
+
+		grpOptions = new FlxTypedGroup<Alphabet>();
+		add(grpOptions);
+
+		for (i in 0...options.length)
+		{
+			var optionText:Alphabet = new Alphabet(0, 0, options[i], true, false);
+			optionText.screenCenter();
+			optionText.y += (100 * (i - (options.length / 2))) + 50;
+			grpOptions.add(optionText);
+		}
+
+		changeSelection();
+
+		super.create();
+	}
+
+	override function closeSubState() {
+		super.closeSubState();
+	}
+
+	override function update(elapsed:Float) {
+		super.update(elapsed);
+
+		if (controls.UP_P) {
+			changeSelection(-1);
+		}
+		if (controls.DOWN_P) {
+			changeSelection(1);
+		}
+
+		if (controls.RESET)
+			FlxG.save.data.offset = 0;
+
+		if (controls.BACK) {
+			FlxG.sound.play(Paths.sound('cancelMenu'));
+			MusicBeatState.switchState(new MainMenuState());
+		}
+
+		if (controls.ACCEPT) {
+			openSelectedSubstate(options[curSelected]);
+		}
+	}
+	
+	function changeSelection(change:Int = 0) {
+		curSelected += change;
+		if (curSelected < 0)
+			curSelected = options.length - 1;
+		if (curSelected >= options.length)
+			curSelected = 0;
+
+		var bullShit:Int = 0;
+
+		for (item in grpOptions.members) {
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+
+			item.alpha = 0.6;
+			if (item.targetY == 0) {
+				item.alpha = 1;
+			}
+		}
+		FlxG.sound.play(Paths.sound('scrollMenu'));
+	}
+}
